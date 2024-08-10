7669336f002b942dc85b4cc401aa119c23dc9e82
Shrey0207 <shreytolasaria4297@gmail.com>
2024-08-10 14:51:01 +0530

    Fix RideCard layout: Align "Request Ride" button in line with "Price" and other elements using SimpleGrid

--- client/src/components/User/RideCard/index.jsx
+++ client/src/components/User/RideCard/index.jsx
@@ -35,7 +35,7 @@ const RideCard = props => {
         mx={['1rem', '2rem', '3rem', '4rem']}
         width="80vw"
         borderRadius="16px"
-        boxShadow=" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+        boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
         height={{ xs: '400px', sm: '300px', md: '150px', lg: '150px' }}
       >
         <SimpleGrid columns={[1, 3, 4, 5, 6, 7]} spacing="40px">
